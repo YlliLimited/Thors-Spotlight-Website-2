c79915d08ce533f912487bf7bab986c0b69db32d
YlliLimited <yllkelmendi05@gmail.com>
2025-03-03 14:31:49 +0100

    modified:   Assets/Videos/thors intro per web.mp4 	modified:   CSS/styles.css 	modified:   JS/script.js 	modified:   about/index.html

--- JS/script.js
+++ JS/script.js
@@ -43,78 +43,81 @@ document.addEventListener("mousemove", (e) => {
 
 // Loading Video
 const video = document.getElementById("loadingVideo");
-const videoBackground = document.getElementById("loadingVideoBackground");
-const closeButton = document.getElementById("closeButton");
-let videoPlayTimes = sessionStorage.getItem("videoPlayTimes") || "0";
 
-window.addEventListener("load", () => {
-    if (videoPlayTimes === "1") {
+if (video) {
+    const videoBackground = document.getElementById("loadingVideoBackground");
+    const closeButton = document.getElementById("closeButton");
+    let videoPlayTimes = sessionStorage.getItem("videoPlayTimes") || "0";
+
+    window.addEventListener("load", () => {
+        if (videoPlayTimes === "1") {
+            video.style.visibility = "hidden";
+            videoBackground.style.visibility = "hidden";
+            video.style.animation = "none";
+            videoBackground.style.animation = "none";
+            video.pause();
+        } else {
+            checkUserAgentVideoAutoplay();
+        }
+    });
+
+    const checkUserAgentVideoAutoplay = async () => {
+        try {
+            await video.play();
+            video.style.animation = "loadingVideoAnimation 17s linear";
+            videoBackground.style.animation =
+                "loadingVideoAnimation 17s linear";
+            video.setAttribute("autoplay", true);
+            sessionStorage.setItem("videoPlayTimes", "1");
+        } catch (err) {
+            sessionStorage.setItem("videoPlayTimes", "1");
+            video.setAttribute("autoplay", false);
+            video.setAttribute("controls", true);
+            video.onplay = () => {
+                video.style.visibility = "visible";
+                videoBackground.style.visibility = "visible";
+                video.style.animation = "none";
+                videoBackground.style.animation = "none";
+            };
+            video.onended = () => {
+                video.style.visibility = "hidden";
+                videoBackground.style.visibility = "hidden";
+                video.style.animation = "loadingVideoAnimation 2s linear";
+                videoBackground.style.animation =
+                    "loadingVideoAnimation 2s linear";
+            };
+        }
+    };
+
+    video.addEventListener("ended", () => {
+        sessionStorage.setItem("videoPlayTimes", "1");
+    });
+
+    closeButton.addEventListener("click", () => {
         video.style.visibility = "hidden";
         videoBackground.style.visibility = "hidden";
         video.style.animation = "none";
         videoBackground.style.animation = "none";
         video.pause();
-    } else {
-        checkUserAgentVideoAutoplay();
-    }
-});
-
-const checkUserAgentVideoAutoplay = async () => {
-    try {
-        await video.play();
-        video.style.animation = "loadingVideoAnimation 17s linear";
-        videoBackground.style.animation = "loadingVideoAnimation 17s linear";
-        video.setAttribute("autoplay", true);
         sessionStorage.setItem("videoPlayTimes", "1");
-    } catch (err) {
-        sessionStorage.setItem("videoPlayTimes", "1");
-        video.setAttribute("autoplay", false);
-        video.setAttribute("controls", true);
-        video.onplay = () => {
-            video.style.visibility = "visible";
-            videoBackground.style.visibility = "visible";
-            video.style.animation = "none";
-            videoBackground.style.animation = "none";
-        };
-        video.onended = () => {
-            video.style.visibility = "hidden";
-            videoBackground.style.visibility = "hidden";
-            video.style.animation = "loadingVideoAnimation 2s linear";
-            videoBackground.style.animation = "loadingVideoAnimation 2s linear";
-        };
-    }
-};
-
-video.addEventListener("ended", () => {
-    sessionStorage.setItem("videoPlayTimes", "1");
-});
-
-closeButton.addEventListener("click", () => {
-    video.style.visibility = "hidden";
-    videoBackground.style.visibility = "hidden";
-    video.style.animation = "none";
-    videoBackground.style.animation = "none";
-    video.pause();
-    sessionStorage.setItem("videoPlayTimes", "1");
-});
+    });
+}
 // Loading Video End
 
+// start video at frame 0
+const frameNumber = 0;
 
+// lower numbers = faster playback
+const playbackConst = 400;
 
- // start video at frame 0
- var frameNumber = 0,
-        
- // lower numbers = faster playback
- playbackConst = 800, 
-
- // select video element         
- vid = document.getElementById('scrollingVideo'); 
+// select video element
+const vid = document.getElementById("scrollingVideo");
 
 // Use requestAnimationFrame for smooth playback
-function scrollPlay(){  
-var frameNumber  = window.scrollY/playbackConst;
-vid.currentTime  = frameNumber;
-window.requestAnimationFrame(scrollPlay);
+function scrollPlay() {
+    let frameNumber = window.scrollY / playbackConst;
+    vid.currentTime = frameNumber;
+    window.requestAnimationFrame(scrollPlay);
 }
 
 window.requestAnimationFrame(scrollPlay);
